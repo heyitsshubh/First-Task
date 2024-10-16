4d5501a94da42f13a05bf3746dab6bd5f008aac7
unknown <you@example.com>
2024-10-16 22:47:05 +0530

    add payment button and footer section ,add hover effect on menuitems

--- add.js
+++ add.js
@@ -113,3 +113,15 @@ currentProductColors.forEach((color, position) => {
       currentProductImg.src = choosenProduct.colors[position].img;
     });
   });
+
+  const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
